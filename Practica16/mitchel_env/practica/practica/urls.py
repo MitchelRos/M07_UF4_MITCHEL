ca58b0fffce88112bd052243c8a383b1d75a62e4
MitchelRos <113449451+MitchelRos@users.noreply.github.com>
2023-04-13 20:27:36 +0200

    Introduccion de formulario (#2)

--- Practica16/mitchel_env/practica/practica/urls.py
+++ Practica16/mitchel_env/practica/practica/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('prapp/', include('practicApp.urls')),
+    path('', include('practicApp.urls')),
     path('admin/', admin.site.urls),
 ]
