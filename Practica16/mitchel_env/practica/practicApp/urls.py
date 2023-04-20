e6f090bb07c7c5d020d932bd3f77dce6243700a9
MitchelRos <mrosado19@jaumebalmes.net>
2023-04-20 19:19:16 +0200

    Update hecho?? ¯\(°_o)/¯

--- Practica16/mitchel_env/practica/practicApp/urls.py
+++ Practica16/mitchel_env/practica/practicApp/urls.py
@@ -4,5 +4,5 @@ from. import views
 urlpatterns = [
      path('', views.index, name='index'),
      path('form', views.form, name='form'),
-
+     path('update-user/<str:pk>/', views.update_user, name='update-user'),
 ]
